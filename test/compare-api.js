e8a825f27e9dcc0f532b0733bfd714c10ed081ae
Ben Toner <bqp@bentoner.com>
2017-11-26 16:26:10 +1100

    add simple tests

---
+++ test/compare-api.js
@@ -0,0 +1,96 @@
+// These tests run against a test account on the live API. This ensures that everything is still
+// working on both the client and on the server.
+
+var chai = require('chai');
+var chaiAsPromised = require('chai-as-promised');
+var fs = require('fs');
+
+chai.use(chaiAsPromised);
+chai.should();
+
+describe('Compare Api Node Client live tests', function() {
+    this.timeout(10000);
+
+    before(function() {
+        // These credentials are for the Draftable Comparison API Test Account and can be public.
+        this.client = require('..').client('GOiDaN-test', 'fe055b5a54c4d58264f70050a469536e');
+        this.comparisons = this.client.comparisons;
+        this.identifier = this.comparisons.generateIdentifier();
+        this.identifier2 = this.comparisons.generateIdentifier();
+    });
+
+    after(function() {
+        this.comparisons.destroy(this.identifier);
+        this.comparisons.destroy(this.identifier2);
+    });
+
+    describe('Create comparison from URLs', function() {
+
+        step('create the comparison', function() {
+            var request = this.comparisons.create({
+                left: {
+                    source: 'https://api.draftable.com/static/test-documents/code-of-conduct/left.rtf',
+                    fileType: 'rtf'
+                },
+                right: {
+                    source: 'https://api.draftable.com/static/test-documents/code-of-conduct/right.pdf',
+                    fileType: 'pdf'
+                },
+                identifier: this.identifier
+            });
+            return Promise.all([
+                request.should.eventually.be.fulfilled,
+                request.should.eventually.have.property('identifier', this.identifier)
+            ]);
+        });
+
+        step('retrieve the comparison', function() {
+            var request = this.comparisons.get(this.identifier);
+            return Promise.all([
+                request.should.eventually.be.fulfilled,
+                request.should.eventually.have.property('identifier', this.identifier)
+            ]);
+        });
+
+        step('generate a public viewer URL', function() {
+            var viewerURL = this.comparisons.publicViewerURL(this.identifier);
+            viewerURL.should.be.a('string');
+            viewerURL.should.not.be.empty;
+        });
+
+        step('generate a signed viewer URL', function() {
+            var viewerURL = this.comparisons.signedViewerURL(this.identifier);
+            viewerURL.should.be.a('string');
+            viewerURL.should.not.be.empty;
+        });
+
+        step('delete the comparison', function() {
+            this.comparisons.destroy(this.identifier).then(function() {
+                var request = this.comparisons.get(this.identifier);
+                return Promise.all([
+                    request.should.eventually.be.rejected
+                ]);
+            });
+        });
+    });
+
+    describe('Create comparison from files', function() {
+        it('should create a comparison', function() {
+            var request = this.comparisons.create({
+                left: {
+                    source: fs.readFileSync('test/assets/left.rtf'),
+                    fileType: 'rtf'
+                },
+                right: {
+                    source: fs.readFileSync('test/assets/right.pdf'),
+                    fileType: 'pdf'
+                },
+                identifier: this.identifier2
+            });
+            return Promise.all([
+                request.should.eventually.be.fulfilled,
+                request.should.eventually.have.property('identifier', this.identifier2)
+            ]);
+        });
+    });
+});
